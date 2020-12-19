3c010b789900396d65f4c02a28e62b380cd9ac1a
aketchum94 <alexketchum94@gmail.com>
2020-12-19 00:35:24 -0500

    build homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>
+      My name is alex and I made this.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Hi There'
+    }
+  }
+}
+</script>
