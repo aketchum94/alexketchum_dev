4ec945f48c12a6e033b0132017cf5df3dcafd6eb
aketchum94 <alexketchum94@gmail.com>
2021-05-13 20:28:07 -0400

    add store

---
+++ src/store/modules/blog.js
@@ -0,0 +1,18 @@
+//initial state
+const state = () => ({
+  val: ''
+})
+
+const actions = {}
+
+const getters = {}
+
+const mutations = {}
+
+export default {
+  namespaced: true,
+  state,
+  actions,
+  getters,
+  mutations
+}
